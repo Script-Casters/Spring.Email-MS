f42153829194a034dcb0cb1ea3d0005e12803bce
Frankie Rodriguez <rfrankie04@gmail.com>
2020-11-17 23:58:46 -0500

    Swagger works and added MS name field for template generation compatibility

--- src/main/java/com/scriptcasters/services/email/EmailApplication.java
+++ src/main/java/com/scriptcasters/services/email/EmailApplication.java
@@ -1,21 +1,33 @@
 package com.scriptcasters.services.email;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@RestController
 public class EmailApplication {
+
+	@Value("${com.scriptcasters.ms.name}")
+	private String msName;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmailApplication.class, args);
 	}
 
 	@GetMapping("/")
-	public ResponseEntity<?> getWelcomeMessage(){
+	public ResponseEntity<?> getHelloMessage(){
 		return new ResponseEntity<>("Hello!", HttpStatus.OK);
+	}
+
+	@GetMapping("/welcome")
+	public ResponseEntity<?> getWelcomeMessage(){
+		return new ResponseEntity<>("Welcome to the " + msName + " made by Script Casters! If you have gotten this far, good job... but I must warn... it is a dark road ahead. Please be sure to GET plenty of REST!", HttpStatus.OK);
 	}
 
 }
--- src/main/java/com/scriptcasters/services/email/respository/GeneralMailLogRepository.java
+++ src/main/java/com/scriptcasters/services/email/respository/GeneralMailLogRepository.java
@@ -2,7 +2,8 @@ package com.scriptcasters.services.email.respository;
 
 import com.scriptcasters.services.email.model.GeneralMailLog;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface GeneralMailLogRepository extends CrudRepository<GeneralMailLog, Long> {
 }
